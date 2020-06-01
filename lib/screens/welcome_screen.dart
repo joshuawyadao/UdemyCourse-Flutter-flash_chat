1df066c05e7c5cdb16c1667b02378afa610ecc78
jwy4877 <jwy4877@g.rit.edu>
2020-06-01 10:45:04 -0700

    + color tween to animate the background from bluegrey to white
    - curved animation for demonstration purposes

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -23,20 +23,12 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       duration: Duration(seconds: 1),
     );
 
-    animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.decelerate,
-    );
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(controller);
 
     controller.forward();
-
-    animation.addStatusListener((status) {
-//      if (status == AnimationStatus.completed) {
-//        controller.reverse(from: 1.0);
-//      } else if (status == AnimationStatus.dismissed) {
-//        controller.forward();
-//      }
-    });
 
     controller.addListener(() {
       setState(() {});
@@ -54,7 +46,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
